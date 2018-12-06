6ebe2a1e60a02a329047a245a8edc482e202e0ae
Josh Rabinowitz <joshr@joshr.com>
2018-12-06 13:24:56 -0500

    setup and user D:P:Database

--- lib/DancerDBTest.pm
+++ lib/DancerDBTest.pm
@@ -1,10 +1,17 @@
 package DancerDBTest;
+use Dancer::Plugin::Database;
 use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
+};
+
+get '/database' => sub {
+    my $dbname = database()->selectrow_array( "select database()" );
+    template 'index', { content=>"HI, connected to $dbname" };
+
 };
 
 true;
